6e602744ad5c7c6bf8c01e7b814ed0f671a50c64
Auric Vente <mprops@protonmail.com>
2023-04-27 05:11:26 -0600

    Mods

--- js/base.js
+++ js/base.js
@@ -1,24 +1,24 @@
 const Paste = {}
 
-Paste.ls_tokens = "tokens_v1"
-Paste.ls_passwd = "passwd_v1"
+Paste.ls_tokens = `tokens_v1`
+Paste.ls_passwd = `passwd_v1`
 Paste.max_content_length = 500000
 Paste.max_comment_length = 200
 Paste.is_loading = true
 Paste.leaving = false
 Paste.updated = false
 
-Paste.default_comment = "Leave a comment here"
+Paste.default_comment = `Leave a comment here`
 
-Paste.init = function () {
-	Paste.main = document.getElementById("paste_main")
-	Paste.textarea = document.getElementById("paste_textarea")
-	Paste.footer = document.getElementById("paste_footer")
-	Paste.comment_content = document.getElementById("paste_comment_content")
-	Paste.loading = document.getElementById("paste_loading")
-	Paste.toolbar_save = document.getElementById("paste_toolbar_save")
-	Paste.toolbar_update = document.getElementById("paste_toolbar_update")
-	Paste.toolbar_new = document.getElementById("paste_toolbar_new")
+Paste.init = () => {
+	Paste.main = document.getElementById(`paste_main`)
+	Paste.textarea = document.getElementById(`paste_textarea`)
+	Paste.footer = document.getElementById(`paste_footer`)
+	Paste.comment_content = document.getElementById(`paste_comment_content`)
+	Paste.loading = document.getElementById(`paste_loading`)
+	Paste.toolbar_save = document.getElementById(`paste_toolbar_save`)
+	Paste.toolbar_update = document.getElementById(`paste_toolbar_update`)
+	Paste.toolbar_new = document.getElementById(`paste_toolbar_new`)
 
 	Paste.check_save()
 	Paste.create_editor()
@@ -31,16 +31,16 @@ Paste.init = function () {
 	Paste.stop_loading_mode()
 	Paste.setup_click_events()
 
-	Paste.editor.setOption("mode", "clike")
+	Paste.editor.setOption(`mode`, `clike`)
 	Paste.editor.refresh()
 	Paste.editor.focus()
 }
 
-Paste.create_editor = function () {
+Paste.create_editor = () => {
 	Paste.editor = CodeMirror.fromTextArea(Paste.textarea,
 		{
 			lineNumbers: true,
-			theme: "nord",
+			theme: `nord`,
 			indentWithTabs: true,
 			tabSize: 4,
 			lineWrapping: true,
@@ -51,11 +51,11 @@ Paste.create_editor = function () {
 	Paste.set_content(Paste.initial_content)
 }
 
-Paste.update_paste = function () {
+Paste.update_paste = () => {
 	Paste.save_paste(true)
 }
 
-Paste.paste_is_empty = function () {
+Paste.paste_is_empty = () => {
 	let content = Paste.get_content(true)
 	let comment = Paste.get_comment()
 
@@ -66,7 +66,7 @@ Paste.paste_is_empty = function () {
 	return false
 }
 
-Paste.save_paste = function (update = false) {
+Paste.save_paste = (update = false) => {
 	if (update && !Paste.owner) {
 		return false
 	}
@@ -77,17 +77,17 @@ Paste.save_paste = function (update = false) {
 	content = Paste.untab_string(content)
 
 	if (Paste.paste_is_empty()) {
-		Paste.show_footer_message("Can't Save An Empty Paste", false)
+		Paste.show_footer_message(`Can't Save An Empty Paste`, false)
 		return false
 	}
 
 	if (content.length > Paste.max_content_length) {
-		Paste.show_footer_message("Paste Is Too Big", false)
+		Paste.show_footer_message(`Paste Is Too Big`, false)
 		return false
 	}
 
 	if (Paste.posting) {
-		Paste.show_footer_message("Paste Save Already In Progress", false)
+		Paste.show_footer_message(`Paste Save Already In Progress`, false)
 		return false
 	}
 
@@ -95,13 +95,14 @@ Paste.save_paste = function (update = false) {
 
 	if (update) {
 		token = Paste.get_token_by_url(Paste.code)
-	} else {
-		token = ""
+	}
+	else {
+		token = ``
 	}
 
-	let onsuccess = function (response) {
-		let code = response["code"]
-		let token = response["token"]
+	let onsuccess = (response) => {
+		let code = response[`code`]
+		let token = response[`token`]
 
 		if (code && token) {
 			Paste.push_to_tokens(code, token)
@@ -110,13 +111,14 @@ Paste.save_paste = function (update = false) {
 		if (code) {
 			if (code === Paste.code) {
 				Paste.after_update()
-			} else {
+			}
+			else {
 				Paste.go_to_location(`?code=${code}&saved=true`)
 			}
 		}
 	}
 
-	let passwd = ""
+	let passwd = ``
 	let passwdobj = Paste.get_local_storage(Paste.ls_passwd)
 
 	if (passwdobj) {
@@ -124,55 +126,55 @@ Paste.save_paste = function (update = false) {
 	}
 
 	if (!passwd) {
-		passwd = prompt("Enter Password")
+		passwd = prompt(`Enter Password`)
 		Paste.save_local_storage(Paste.ls_passwd, {passwd: passwd})
 	}
 
-	Paste.send_post("save.php",
+	Paste.send_post(`save.php`,
 		{
-			content: content, 
-			comment: Paste.get_comment(), 
+			content: content,
+			comment: Paste.get_comment(),
 			token: token,
 			passwd: passwd
 		}, onsuccess)
 }
 
-Paste.send_post = function (target, data, onsuccess) {
+Paste.send_post = (target, data, onsuccess) => {
 	Paste.posting = true
 
 	let XHR = new XMLHttpRequest()
-	let urlEncodedData = ""
+	let urlEncodedData = ``
 	let urlEncodedDataPairs = []
 
-	XHR.responseType = "json"
+	XHR.responseType = `json`
 
 	// Turn the data object into an array of URL-encoded key/value pairs.
 	for (let name in data) {
-		urlEncodedDataPairs.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]))
+		urlEncodedDataPairs.push(encodeURIComponent(name) + `=` + encodeURIComponent(data[name]))
 	}
 
-	// Combine the pairs into a single string and replace all %-encoded spaces to 
+	// Combine the pairs into a single string and replace all %-encoded spaces to
 	// the '+' character; matches the behaviour of browser form submissions.
-	urlEncodedData = urlEncodedDataPairs.join("&").replace(/%20/g, "+");
+	urlEncodedData = urlEncodedDataPairs.join(`&`).replace(/%20/g, `+`);
 
 	// Define what happens on successful data submission
-	XHR.addEventListener("load", function (event, data) {
+	XHR.addEventListener(`load`, (event, data) => {
 		Paste.posting = false
 	})
 
 	// Define what happens in case of error
-	XHR.addEventListener("error", function (event) {
+	XHR.addEventListener(`error`, (event) => {
 		Paste.posting = false
-		console.error("Oops! Something goes wrong.")
+		console.error(`Oops! Something goes wrong.`)
 	})
 
 	// Set up our request
-	XHR.open("POST", target)
+	XHR.open(`POST`, target)
 
 	// Add the required HTTP header for form data POST requests
-	XHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+	XHR.setRequestHeader(`Content-Type`, `application/x-www-form-urlencoded`)
 
-	XHR.onreadystatechange = function (aEvt) {
+	XHR.onreadystatechange = (aEvt) => {
 		if (XHR.readyState == 4) {
 			if (XHR.status == 200) {
 				if (XHR.response) {
@@ -181,8 +183,8 @@ Paste.send_post = function (target, data, onsuccess) {
 				}
 			}
 
-			console.info("XHR Error")
-			Paste.save_local_storage(Paste.ls_passwd, {passwd: ""})
+			console.info(`XHR Error`)
+			Paste.save_local_storage(Paste.ls_passwd, {passwd: ``})
 		}
 	}
 
@@ -190,91 +192,94 @@ Paste.send_post = function (target, data, onsuccess) {
 	XHR.send(urlEncodedData)
 }
 
-Paste.show_footer_message = function (s, succ) {
+Paste.show_footer_message = (s, succ) => {
 	clearTimeout(Paste.footer_timeout)
 
 	Paste.footer.innerHTML = s
 
 	if (succ) {
-		Paste.footer.style.backgroundColor = "#5bab70"
-	} else {
-		Paste.footer.style.backgroundColor = "#c05f5f"
+		Paste.footer.style.backgroundColor = `#5bab70`
+	}
+	else {
+		Paste.footer.style.backgroundColor = `#c05f5f`
 	}
 
-	Paste.footer.style.bottom = "0"
+	Paste.footer.style.bottom = `0`
 
-	Paste.footer_timeout = setTimeout(function () {
-		Paste.footer.style.bottom = "-2.5rem"
+	Paste.footer_timeout = setTimeout(() => {
+		Paste.footer.style.bottom = `-2.5rem`
 	}, 3000)
 }
 
-Paste.check_initial_token = function () {
+Paste.check_initial_token = () => {
 	if (Paste.code && Paste.token) {
 		Paste.push_to_tokens(Paste.code, Paste.token)
 	}
 }
 
-Paste.remove_get_parameters_from_url = function () {
-	let url = window.location.href.split("&")[0]
+Paste.remove_get_parameters_from_url = () => {
+	let url = window.location.href.split(`&`)[0]
 	Paste.change_url(url)
 }
 
-Paste.change_url = function (url, replace = true) {
+Paste.change_url = (url, replace = true) => {
 	if (replace) {
-		window.history.replaceState("object", document.title, url)
-	} else {
-		window.history.pushState("object", document.title, url)
+		window.history.replaceState(`object`, document.title, url)
+	}
+	else {
+		window.history.pushState(`object`, document.title, url)
 	}
 }
 
-Paste.after_update = function () {
+Paste.after_update = () => {
 	Paste.updated = true
 	Paste.initial_content = Paste.get_content()
 	Paste.initial_comment = Paste.get_comment()
 	Paste.show_save_success(true)
 }
 
-Paste.get_local_storage = function (ls_name) {
+Paste.get_local_storage = (ls_name) => {
 	let obj
 
 	if (localStorage[ls_name]) {
 		try {
 			obj = JSON.parse(localStorage.getItem(ls_name))
 		}
-
 		catch (err) {
 			console.error(err)
 			obj = null
 		}
-	} else {
+	}
+	else {
 		obj = null
 	}
 
 	return obj
 }
 
-Paste.save_local_storage = function (ls_name, obj) {	
+Paste.save_local_storage = (ls_name, obj) => {
 	obj = JSON.stringify(obj)
 	localStorage.setItem(ls_name, obj)
 }
 
-Paste.go_to_location = function (url) {
+Paste.go_to_location = (url) => {
 	Paste.leaving = true
 	window.location.href = url
 }
 
-Paste.new_paste = function () {
+Paste.new_paste = () => {
 	if (Paste.code) {
-		let url = window.location.href.split("?")[0]
+		let url = window.location.href.split(`?`)[0]
 		Paste.go_to_location(url)
-	} else {
-		Paste.set_content("")
-		Paste.set_comment("")
+	}
+	else {
+		Paste.set_content(``)
+		Paste.set_comment(``)
 		Paste.editor.focus()
 	}
 }
 
-Paste.paste_is_modified = function () {
+Paste.paste_is_modified = () => {
 	if
 		(
 		Paste.get_content() === Paste.initial_content &&
@@ -286,7 +291,7 @@ Paste.paste_is_modified = function () {
 	return true
 }
 
-Paste.get_content = function (trim = false) {
+Paste.get_content = (trim = false) => {
 	let value = Paste.document.getValue()
 
 	if (trim) {
@@ -296,55 +301,56 @@ Paste.get_content = function (trim = false) {
 	return value
 }
 
-Paste.set_content = function (s) {
+Paste.set_content = (s) => {
 	Paste.document.setValue(s)
 }
 
-Paste.setup_comment = function () {
+Paste.setup_comment = () => {
 	Paste.comment_content.innerText = Paste.initial_comment
 
-	Paste.comment_content.addEventListener("blur", function () {
+	Paste.comment_content.addEventListener(`blur`, () => {
 		Paste.set_comment(Paste.get_comment())
 	})
 }
 
-Paste.clean_string2 = function (s) {
-	return s.replace(/\s+/g, " ").trim()
+Paste.clean_string2 = (s) => {
+	return s.replace(/\s+/g, ` `).trim()
 }
 
-Paste.get_comment = function () {
+Paste.get_comment = () => {
 	return Paste.clean_string2(Paste.comment_content.innerText).substring(0, Paste.max_comment_length).trim()
 }
 
-Paste.set_comment = function (val) {
+Paste.set_comment = (val) => {
 	Paste.comment_content.innerText = val
 }
 
-Paste.setup_window_load = function () {
-	window.onbeforeunload = function () {
+Paste.setup_window_load = () => {
+	window.onbeforeunload = () => {
 		if (!Paste.leaving && Paste.paste_is_modified()) {
-			return "Are you sure?"
-		} else {
+			return `Are you sure?`
+		}
+		else {
 			Paste.start_loading_mode(true)
 		}
 	}
 }
 
-Paste.stop_loading_mode = function() {
-	Paste.loading.style.display = "none"
+Paste.stop_loading_mode = () => {
+	Paste.loading.style.display = `none`
 	Paste.is_loading = false
 }
 
-Paste.start_loading_mode = function() {	
-	Paste.loading.style.display = "flex"
+Paste.start_loading_mode = () => {
+	Paste.loading.style.display = `flex`
 	Paste.is_loading = true
 }
 
-Paste.get_token_by_url = function (url) {
-	return Paste.tokens.items[url] || ""
+Paste.get_token_by_url = (url) => {
+	return Paste.tokens.items[url] || ``
 }
 
-Paste.get_tokens = function () {
+Paste.get_tokens = () => {
 	Paste.tokens = Paste.get_local_storage(Paste.ls_tokens)
 
 	if (Paste.tokens === null) {
@@ -364,11 +370,11 @@ Paste.get_tokens = function () {
 	}
 }
 
-Paste.save_tokens = function () {
+Paste.save_tokens = () => {
 	Paste.save_local_storage(Paste.ls_tokens, Paste.tokens)
 }
 
-Paste.push_to_tokens = function (url, token, save = true) {
+Paste.push_to_tokens = (url, token, save = true) => {
 	Paste.tokens.items[url] = token
 
 	if (save) {
@@ -376,33 +382,35 @@ Paste.push_to_tokens = function (url, token, save = true) {
 	}
 }
 
-Paste.check_ownership = function () {
+Paste.check_ownership = () => {
 	if (Paste.get_token_by_url(Paste.code)) {
 		Paste.owner = true
-		Paste.toolbar_update.classList.remove("paste_disabled")
-	} else {
+		Paste.toolbar_update.classList.remove(`paste_disabled`)
+	}
+	else {
 		Paste.owner = false
-		Paste.toolbar_update.classList.add("paste_disabled")
+		Paste.toolbar_update.classList.add(`paste_disabled`)
 	}
 }
 
-Paste.show_save_success = function (update = false) {
+Paste.show_save_success = (update = false) => {
 	if (update) {
-		Paste.show_footer_message("Paste Updated", true)
-	} else {
-		Paste.show_footer_message("Paste Saved", true)
+		Paste.show_footer_message(`Paste Updated`, true)
+	}
+	else {
+		Paste.show_footer_message(`Paste Saved`, true)
 	}
 }
 
-Paste.check_save = function () {
+Paste.check_save = () => {
 	if (Paste.saved) {
 		Paste.show_save_success()
 	}
 }
 
-Paste.untab_string = function (s) {
-	s = s.replace(/\t/gm, "  ")
-	let lines = s.split("\n")
+Paste.untab_string = (s) => {
+	s = s.replace(/\t/gm, `  `)
+	let lines = s.split(`\n`)
 
 	if (lines.length <= 1) {
 		return s
@@ -417,45 +425,46 @@ Paste.untab_string = function (s) {
 		}
 
 		let m = line.match(/^\s+/)
-		
+
 		if (m) {
 			let n = m[0].length
 
 			if (pos === -1 || n < pos) {
 				pos = n
 			}
-			
+
 			ns.push(n)
-		} else {
+		}
+		else {
 			return s
 		}
 	}
-	
+
 	let new_lines = []
-	let spaces = ""
-		
+	let spaces = ``
+
 	for (let i=0; i<pos; i++) {
-		spaces += " "
+		spaces += ` `
 	}
-	
-	for (let line of lines) {    
+
+	for (let line of lines) {
 		let re = new RegExp(`(^${spaces})`)
-		new_lines.push(line.replace(re, ""))
+		new_lines.push(line.replace(re, ``))
 	}
-	
-	return new_lines.join("\n")
+
+	return new_lines.join(`\n`)
 }
 
-Paste.setup_click_events = function () {
-	Paste.toolbar_save.addEventListener("click", function () {
+Paste.setup_click_events = () => {
+	Paste.toolbar_save.addEventListener(`click`, () => {
 		Paste.save_paste()
 	})
 
-	Paste.toolbar_update.addEventListener("click", function () {
+	Paste.toolbar_update.addEventListener(`click`, () => {
 		Paste.update_paste()
 	})
 
-	Paste.toolbar_new.addEventListener("click", function () {
+	Paste.toolbar_new.addEventListener(`click`, () => {
 		Paste.new_paste()
 	})
 }
